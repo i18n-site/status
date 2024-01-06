c0c6c8b42d084875bf8e2e44b3fedd483efac425
i18n <i18n.site@gmail.com>
2024-01-06 10:18:40 +0800

    ♨

--- src/Status.svelte
+++ src/Status.svelte
@@ -1,33 +1,36 @@
 <template lang="pug">
 +if check
-  table.e
+  main
     +if err.length
-      +each err as [kind, hli]
-        tr
-          th(colspan=2) { kind }
-          th 出错次数
-          th 持续时长
-        +each hli as [host, li]
-          +each li as [ip, err, ts], p
-            tr
-              +if p == 0
-                td(rowspan:li.length) { host }
-              td IPV{ip}
-              td {err}
-              td {ts}
-  table
+      table.e
+        +each err as [kind, hli]
+          tr
+            th(colspan=2) { kind }
+            th 出错次数
+            th 持续分钟
+          +each hli as [host, li]
+            +each li as [ip, err, ts], p
+              tr
+                +if p == 0
+                  td(rowspan:li.length) { host }
+                td IrV{ip}
+                td {err}
+                td {-ts}
     +if ok.length
-      +each ok as [kind, hli]
-        tr
-          th(colspan=2) { kind }
-          th 下次检查
-        +each hli as [host, li]
-          +each li as [ip, err, ts], p
-            tr
-              +if p == 0
-                td(rowspan:li.length) { host }
-              td IPV{ip}
-              td {ts}
+      table
+        +each ok as [kind, hli]
+          tr
+            th(colspan=2) { kind }
+            th
+              b 下次检查
+              b 单位分钟
+          +each hli as [host, li]
+            +each li as [ip, err, ts], p
+              tr
+                +if p == 0
+                  td(rowspan:li.length) { host }
+                td IPV{ip}
+                td {ts}
   +else
     WAIT
 
@@ -81,6 +84,11 @@ table
   &.e
     color red
     margin-bottom 0
+
+  th
+    &>b
+      display block
+      font-size 12px
 
 :global(table>tr>td, table>tr>th)
   border 1px solid #999
