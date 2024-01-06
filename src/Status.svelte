dc1a94411bf7c44be0342045aaad8175caadeb6b
i18n <i18n.site@gmail.com>
2024-01-06 16:21:42 +0800

    ♨

--- src/Status.svelte
+++ src/Status.svelte
@@ -1,37 +1,42 @@
 <template lang="pug">
 +if check
   main
-    h1 距离上次检查 {last} 分钟
-    +if err.length
-      +each err as [kind, hli]
-        table.e
-          tr
-            th(colspan=2) { kind }
-            th 出错次数
-            th 持续分钟
-          +each hli as [host, li]
-            +each li as [ip, err, ts], p
-              tr
-                +if p == 0
-                  td(rowspan:li.length) { host }
-                td IPV{ip}
-                td {err}
-                td {-ts}
-    +if ok.length
-      +each ok as [kind, hli]
-        table
-          tr
-            th(colspan=2) { kind }
-            th
-              b 下次检查
-              b 单位分钟
-          +each hli as [host, li]
-            +each li as [ip, err, ts], p
-              tr
-                +if p == 0
-                  td(rowspan:li.length) { host }
-                td IPV{ip}
-                td {ts}
+    h1 距离上次检查 {last} 秒
+      b 当前进程累计 {count} 次
+        +if count
+          | / 每次耗时 {cost} 秒
+    - var n = 0;
+    while ++n < 10
+      +if err.length
+        +each err as [kind, hli]
+          table.e
+            tr
+              th(colspan=2) { kind }
+              th 出错次数
+              th 持续分钟
+            +each hli as [host, li]
+              +each li as [ip, err, ts], p
+                tr
+                  +if p == 0
+                    td(rowspan:li.length) { host }
+                  td IPV{ip}
+                  td {err}
+                  td {-ts}
+      +if ok.length
+        +each ok as [kind, hli]
+          table
+            tr
+              th(colspan=2) { kind }
+              th
+                b 下次检查
+                b 单位分钟
+            +each hli as [host, li]
+              +each li as [ip, err, ts], p
+                tr
+                  +if p == 0
+                    td(rowspan:li.length) { host }
+                  td IPV{ip}
+                  td {ts}
   +else
     WAIT
 
@@ -41,7 +46,7 @@
 > @~3/wait:WAIT
   @5-/alive/S.js > Li
 
-+ ok, err, check, last
++ ok, err, check, last, count, cost, begin
 
 get =  =>
   [
@@ -52,14 +57,11 @@ get =  =>
     check
   ] = await Li()
 
-  now = new Date/1000
-  last = Number.parseInt((now - check[0]) / 6)/10
-  console.log {last}
-  # [
-  #   last
-  #   count
-  #   cost
-  # ] = check
+  now = Math.round new Date/1000
+  [begin, count, total_cost] = check
+  last = now - begin
+  if count
+    cost = Math.round(total_cost / count) / 1000
 
   kind = new Map kind
   host = new Map host
@@ -92,13 +94,33 @@ get =  =>
 
   return
 
-onMount get
+timer = setInterval(
+  =>
+    if begin
+      last += 1
+      if last - begin > 60
+        begin = 0
+        try
+         await get()
+        finally
+          begin = last
+    return
+  1000
+)
+onMount =>
+  get()
+  =>
+    clearInterval timer
+    return
+
 </script>
 
 <style lang="stylus">
 main
   display flex
-  flex-flow column wrap
+  flex-direction row
+  flex-wrap wrap
+  overflow auto
 
 :global(table>tr>td, table>tr>th)
   border 1px dashed #ccc
@@ -107,8 +129,15 @@ main
   text-align center
 
 h1
+  display flex
+  flex-direction column
+  font-size 30px
   margin 20px 20px 0
   padding 0
+
+  b
+    font-size 16px
+    margin-top 14px
 
 table
   border 1px solid #ccc
