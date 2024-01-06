53237c697ae4c9896194f23d56dd27e6710da953
i18n <i18n.site@gmail.com>
2024-01-06 16:55:02 +0800

    ♨

--- src/Status.svelte
+++ src/Status.svelte
@@ -1,12 +1,11 @@
 <template lang="pug">
 +if check
-  main
+  i
     h1 距离上次检查 {last} 秒
       b 当前进程累计 {count} 次
         +if count
           | / 每次耗时 {cost} 秒
-    - var n = 0;
-    while ++n < 10
+    i
       +if err.length
         +each err as [kind, hli]
           table.e
@@ -19,24 +18,26 @@
                 tr
                   +if p == 0
                     td(rowspan:li.length) { host }
-                  td IPV{ip}
-                  td {err}
-                  td {-ts}
-      +if ok.length
-        +each ok as [kind, hli]
-          table
-            tr
-              th(colspan=2) { kind }
-              th
-                b 下次检查
-                b 单位分钟
-            +each hli as [host, li]
-              +each li as [ip, err, ts], p
-                tr
-                  +if p == 0
-                    td(rowspan:li.length) { host }
-                  td IPV{ip}
-                  td {ts}
+                  td.s IPV{ip}
+                  td.s {err}
+                  td.s {-ts}
+      - var n = 0;
+      while ++n < 10
+        +if ok.length
+          +each ok as [kind, hli]
+            table
+              tr
+                th(colspan=2) { kind }
+                th
+                  b 下次检查
+                  b 单位分钟
+              +each hli as [host, li]
+                +each li as [ip, err, ts], p
+                  tr
+                    +if p == 0
+                      td(rowspan:li.length) { host }
+                    td.s IPV{ip}
+                    td.s {ts}
   +else
     WAIT
 
@@ -58,8 +59,9 @@ get =  =>
   ] = await Li()
 
   now = Math.round new Date/1000
-  [begin, count, total_cost] = check
-  last = now - begin
+  [ts, count, total_cost] = check
+  begin = last = now - ts
+
   if count
     cost = Math.round(total_cost / count) / 1000
 
@@ -97,13 +99,13 @@ get =  =>
 timer = setInterval(
   =>
     if begin
-      last += 1
-      if last - begin > 60
+      if (last - begin) > 60
         begin = 0
         try
          await get()
         finally
           begin = last
+    last += 1
     return
   1000
 )
@@ -116,11 +118,12 @@ onMount =>
 </script>
 
 <style lang="stylus">
-main
-  display flex
-  flex-direction row
-  flex-wrap wrap
+i
+  font-style normal
   overflow auto
+
+  &>i
+    display flex
 
 :global(table>tr>td, table>tr>th)
   border 1px dashed #ccc
@@ -132,17 +135,18 @@ h1
   display flex
   flex-direction column
   font-size 30px
-  margin 20px 20px 0
+  margin 20px 20px 10px
   padding 0
 
   b
     font-size 16px
-    margin-top 14px
+    margin-top 5px
 
 table
   border 1px solid #ccc
   border-collapse collapse
-  margin 20px
+  float left
+  margin 20px 0 0 20px
 
   &.e
     color red
@@ -150,6 +154,12 @@ table
 
     th
       background #fee
+
+  td
+    font-size 14px
+
+    &.s
+      font-size 12px
 
   th
     background #efe
